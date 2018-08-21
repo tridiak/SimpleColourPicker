90e8feb7c2d8ba0f8dcb5f0ec5f3832b5d7bbfb7
Mark <tridiaknz@gmail.com>
2018-08-22 10:06:04 +1200

    Initial Commit

---
+++ SimpleColourPicker/SimpleColourPicker.h
@@ -0,0 +1,19 @@
+//
+//  SimpleColourPicker.h
+//  SimpleColourPicker
+//
+//  Created by tridiak on 22/08/18.
+//  Copyright © 2018 tridiak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleColourPicker.
+FOUNDATION_EXPORT double SimpleColourPickerVersionNumber;
+
+//! Project version string for SimpleColourPicker.
+FOUNDATION_EXPORT const unsigned char SimpleColourPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleColourPicker/PublicHeader.h>
+
+
